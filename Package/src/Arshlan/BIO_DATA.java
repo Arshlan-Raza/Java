589a29e9085236ef9da4f392f6f82250ceaa255a
Arshlan <arshalanraja@gmail.com>
2023-03-02 21:29:45 +0530

    java code

---
+++ Package/src/Arshlan/BIO_DATA.java
@@ -0,0 +1,12 @@
+package Arshlan;
+
+public class BIO_DATA {
+
+    public static void main(String[] args) {
+
+        USN_4PS21IS007 obj = new USN_4PS21IS007();
+
+        obj.display("Arshlan Raza","4PS21IS007",'A',"Information Science and Engineering (ISE)");
+    }
+
+}
