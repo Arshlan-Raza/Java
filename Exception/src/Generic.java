62ef8e431f7e6a68905341ef9381ca0d1eb67eb7
Arshlan <arshalanraja@gmail.com>
2023-03-12 09:40:10 +0530

    Practice program of java 3rd sem

--- Exception/src/Generic.java
+++ Exception/src/Generic.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Generic {
+class Bound<T extends A>{
+
+    T obj ;
+    Bound(T o){
+        this.obj = o;
+    }
+    public void doRunTest(){
+        this.obj.display();
+    }
+
+
+}
+class A{
+    public void display(){
+        System.out.println("This is class A");
+    }
+}
+class B extends A{
+    public void display(){
+        System.out.println("This is Subclass B");
+    }
+}
+class C extends B{
+    public void display(){
+        System.out.println("This is Subclass C");
+    }
+}
+public class Generic {
+    public static void main(String[] args){
+
+        Bound<A> aobj = new Bound<>(new A());
+        aobj.doRunTest();
+
+        Bound<B> bobj = new Bound<>(new B());
+        bobj.doRunTest();
+
+        Bound<C> cobj = new Bound<>(new C());
+        cobj.doRunTest();
+
+    }
 }
