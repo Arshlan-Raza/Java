2be2b3b9711b738c1bc08d5ae5a750bb9ad3fe41
Arshlan <arshalanraja@gmail.com>
2023-03-20 22:35:37 +0530

    java codes

---
+++ Generic/Thread/Exception.java
@@ -0,0 +1,14 @@
+public class Exception {
+    public static void main(String[] args){
+        int n = 5;
+        int n1 = 0;
+
+        try{
+            System.out.println(a/b);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
+    }
+}
